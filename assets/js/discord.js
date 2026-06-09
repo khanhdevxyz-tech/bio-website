4653085fbe1f98e2ef392c9e5e60c8d11b47ec73
Dinh Khanh <trungvphukpl@gmail.com>
2026-06-09 10:23:55 -0700

    Update discord.js

--- assets/js/discord.js
+++ assets/js/discord.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const userId = "1437446260662472704"; //replace your discord id here
+    const userId = "1500376504335601785"; //replace your discord id here
     const apiUrl = `https://discord-lookup-api-alpha.vercel.app/v1/user/${userId}`;
 
     const avatarFrame = document.getElementById('avatar-frame');
